e2ab39561b91cc2f4fd7fc2bbeb1909cc096825c
MNGJ <rlausmfl256@khu.ac.kr>
2023-05-28 22:26:33 +0900

    2156, 10844 modify

--- BoJ Solving/BoJ Solving/Dynamic Programing/10844 쉬운 계단 수.cpp
+++ BoJ Solving/BoJ Solving/Dynamic Programing/10844 쉬운 계단 수.cpp
@@ -12,24 +12,25 @@ int main()
 
     cin >> N;
     for (int i = 1; i < 10; i++)
-        dp[1][i] = 1;
+        dp[1][i] = 1;  // 자릿수가 한 자릿수 일땐 0일때 제외하고 전부 1
 
     for (int i = 2; i <= N; i++)
     {
         for (int j = 0; j < 10; j++)
         {
-            if (j == 0)
+            if (j == 0)             // 첫째 자리 수가 0일때 자릿수가 -1인 수는 없으므로 [1]만 더함
                 dp[i][j] = dp[i - 1][1];
             else if (j == 9)
-                dp[i][j] = dp[i - 1][8];
+                dp[i][j] = dp[i - 1][8]; // 첫째 자리 수가 9일때 자릿수가 10인 수는 없으므로 [8]만 더함
             else
-                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1];
+                dp[i][j] = dp[i - 1][j - 1] + dp[i - 1][j + 1]; // 현재 첫째 자리 수에 전 단계의 자리수 +- 1 였던 값을 더함 
+            dp[i][j] %= NUM;
         }
     }
 
     for (int i = 0; i < 10; i++)
     {
-        result += dp[N][i] % NUM;
+        result = (result + dp[N][i]) % NUM; // +=로 할 경우 연산 순서가 달라져서 (result + dp[N][i])로 해야함
     }
 
     cout << result;
